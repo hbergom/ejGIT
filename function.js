4510d3764859ec0bed6035575c4d75ba933cee85
hbergom <gomezhernangonzalo@gmail.com>
2020-03-22 13:49:54 -0300

    Agregué los archivos function.js,Index.html y style.css

---
+++ function.js
@@ -0,0 +1,11 @@
+let dameCinco = dameCinco => [1, 2, 3, 4, 5];
+console.log(dameCinco);
+
+let multiplicarPorDos = numero => numero * 2;
+console.log(multiplicarPorDos(123));
+
+/*saludar = nombre => 'Hola, '+ nombre + '!';
+console.log(saludar("Hernan"));*/
+
+let saludar = (nombre, apellido) => "Hola, " + nombre + " " + apellido + "!";
+console.log(saludar("Hernan", "Gomez"));
