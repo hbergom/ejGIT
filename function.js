fe93a96a67b46afcf6c96f5b1080a4fc04a2eca3
hbergom <gomezhernangonzalo@gmail.com>
2020-03-22 15:04:45 -0300

    Se hicieron modificaciones en Function.js, index.html y style.css

--- function.js
+++ function.js
@@ -4,8 +4,8 @@ console.log(dameCinco);
 let multiplicarPorDos = numero => numero * 2;
 console.log(multiplicarPorDos(123));
 
-/*saludar = nombre => 'Hola, '+ nombre + '!';
-console.log(saludar("Hernan"));*/
+saludar = nombre => 'Hola, '+ nombre + '!';
+console.log(saludar("Hernan"));
 
 let saludar = (nombre, apellido) => "Hola, " + nombre + " " + apellido + "!";
 console.log(saludar("Hernan", "Gomez"));
